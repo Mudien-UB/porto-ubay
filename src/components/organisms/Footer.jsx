7392ef08c010d5cb5b199e9b5b1151178627cfc9
Mudien-UB <mudienub@gmail.com>
2026-01-04 13:22:25 +0700

    update

--- src/components/organisms/Footer.jsx
+++ src/components/organisms/Footer.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
   return (
     <>
       <footer className="bg-gray-900 mt-10 text-white py-4">
         <div className="container mx-auto text-center">
-          <p className="text-sm">All rights reserved, © {new Date().getFullYear()}</p>
+          <p className="text-sm">
+            © {new Date().getFullYear()} Mudien_UB.
+            <br /> Third-party assets are used under their respective licenses.
+          </p>
         </div>
       </footer>
     </>
-  )
+  );
 }
--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,38 +1,44 @@
-import React from 'react';
-import { motion } from 'framer-motion';
-import { useInViewAnimation } from '../hooks/useInViewAnimation';
-import { slideInUp } from '../animations/variants'; // asumsikan animasi kamu simpan di utils/animations.js
+import React from "react";
+import { motion } from "framer-motion";
+import { useInViewAnimation } from "../hooks/useInViewAnimation";
+import { slideInUp } from "../animations/variants"; // asumsikan animasi kamu simpan di utils/animations.js
 
 const projects = [
   {
     id: 1,
-    title: 'Toko Onlen API',
-    description: 'Payment and Transaction app for learn',
+    title: "Toko Onlen API",
+    description: "Payment and Transaction app for learn",
     linkRepo: "https://github.com/Mudien-UB/toko-onlen",
-    image: 'https://picsum.photos/id/1/400/300'
+    image: "https://picsum.photos/id/1/400/300",
   },
   {
     id: 2,
-    title: 'Nia Coffee',
-    description: 'Landing Page of Cafe for dummy app',
+    title: "Nia Coffee",
+    description: "Landing Page of Cafe for dummy app",
     linkRepo: "https://github.com/Mudien-UB/Nia-Coffee",
-    image: 'https://picsum.photos/id/3/400/300'
+    image: "https://picsum.photos/id/3/400/300",
   },
   {
     id: 3,
-    title: 'Blogs API with Springboot',
-    description: 'Dummy Project Springboot',
+    title: "Blogs API with Springboot",
+    description: "Dummy Project Springboot",
     linkRepo: "https://github.com/Mudien-UB/java-blogs-restfull-api",
-    image: 'https://picsum.photos/id/2/400/300'
+    image: "https://picsum.photos/id/2/400/300",
   },
   {
     id: 4,
-    title: 'Blogs API with Laravel',
-    description: 'Dummy Project Laravel',
+    title: "Blogs API with Laravel",
+    description: "Dummy Project Laravel",
     linkRepo: "https://github.com/Mudien-UB/laravel-blog-api",
-    image: 'https://picsum.photos/id/4/400/300'
+    image: "https://picsum.photos/id/4/400/300",
   },
-
+  {
+    id: 5,
+    title: "EchoQuotePNG",
+    description: "Just for fun project to type quote and save",
+    linkRepo: "https://github.com/Mudien-UB/EchoQuotePNG",
+    image: "https://picsum.photos/id/5/400/300",
+  },
 ];
 
 export default function ProjectsPage({ id }) {
@@ -40,15 +46,16 @@ export default function ProjectsPage({ id }) {
 
   const openRepo = (link) => {
     window.open(link, "_blank");
-  }
+  };
 
   return (
     <section
       id={id}
       ref={ref}
-      className="w-11/12 mx-auto min-h-screen max-h-max p-10 bg-brunswick-green-100/20 border-2 border-brunswick-green-300 shadow-xl"
-    >
-      <h2 className="text-3xl font-montserrat font-semibold text-center mt-20 mb-6">Recent Projects Repo</h2>
+      className="w-11/12 mx-auto min-h-screen max-h-max p-10 bg-brunswick-green-100/20 border-2 border-brunswick-green-300 shadow-xl">
+      <h2 className="text-3xl font-montserrat font-semibold text-center mt-20 mb-6">
+        Recent Projects Repo
+      </h2>
 
       <div className="grid grid-cols-1 pt-10 px-10 sm:grid-cols-2 md:grid-cols-3 gap-6">
         {projects.map((project, index) => (
@@ -59,8 +66,7 @@ export default function ProjectsPage({ id }) {
             style={{ backgroundImage: `url(${project.image})` }}
             variants={slideInUp(index * 0.5)} // animasi muncul bergantian
             initial="hidden"
-            animate={isInView ? 'visible' : 'hidden'}
-          >
+            animate={isInView ? "visible" : "hidden"}>
             <div className="absolute inset-0 bg-black/50 rounded-xl"></div>
             <div className="relative z-10 text-white">
               <h3 className="text-lg font-semibold">{project.title}</h3>
